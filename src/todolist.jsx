7eec9cd83b4fc62c8f6c81278a7369f1336e726f
cfardin <cfardin51@gmail.com>
2026-03-22 13:27:17 +0600

    todo list complite

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -5,43 +5,79 @@ export default function TodoList() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
-
   const handleInputChange = (event) => {
-    // setNewTask(event.target.value);
+    setNewTask(event.target.value);
   };
 
 
   const addTask = () => {
-
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
   };
 
 
   const deleteTask = (index) => {
-
+    const updatedTasks = tasks.filter((Element, i) => i !== index);
+    setTasks(updatedTasks);
   };
 
 
   const moveTaskUp = (index) => {
-
+    if(index > 0){
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
   };
-
 
   const moveTaskDown = (index) => {
-
+    if(index < tasks.length - 1){
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index+1]] = [updatedTasks[index+1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    } else{
+      return;
+    }
   };
+
+
 
 
   return (
     <div className="to-do">
-        <h1>TodoList</h1>
+      <h1>TodoList</h1>
 
-        <div>
-            <input 
-                type="text" 
-                placeholder="Enter a task" 
-                value={newTask} onChange={handleInputChange}/>
-        </div>
+      <div className="input-box">
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={newTask}
+          onChange={handleInputChange}
+        />
+
+        <button className="add-btn" onClick={addTask}>
+          ➕
+        </button>
+      </div>
+
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-btn" onClick={() => deleteTask(index)}>
+              ❌
+            </button>
+            <button className="move-btn" onClick={() => moveTaskUp(index)}>
+              ☝️
+            </button>
+            <button className="move-btn" onClick={() => moveTaskDown(index)}>
+              👇
+            </button>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
-
